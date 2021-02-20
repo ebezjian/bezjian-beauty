fd74f9a570a137626e8dd758f256b2e5c9e329ae
Ellie Lambert <ebezjian@gmail.com>
2021-02-20 15:50:52 -0500

    add basic styling to product.jsx and  products import to app.js

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
+import useStyles from './styles';
 
 const Product = ({product}) => {
+  const classes = useStyles();
   return (
     <Card className={classes.root}>
       <CardMedia className={classes.media} image='' title={product.name}/>
@@ -24,7 +26,7 @@ const Product = ({product}) => {
           <AddShoppingCart/>
         </IconButton>
       </CardActions>
-      
+
     </Card>
   )
 }
