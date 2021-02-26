ba12941805db8f6e71087969edc623490c29ef3f
Ellie Lambert <ebezjian@gmail.com>
2021-02-26 12:36:14 -0500

    adjust navbar to not be ontop of products display

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
-// import useStyles from './styles';
+import useStyles from './styles';
 
 const products = [
   { id: 1, name : "example 1", description: 'example 1 desc', price : '$5', image: 'https://images.unsplash.com/photo-1596462502278-27bfdc403348?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&w=1000&q=80'},
@@ -10,8 +10,10 @@ const products = [
 ];
 
 const Products = () => {
+  const classes = useStyles();
   return (
-    <main>
+    <main className={classes.content}>
+      <div  className={classes.toolbar}/>
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
