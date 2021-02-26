2caa3ae362d08f5746f7a9dbcec1458bd7600936
Ellie Lambert <ebezjian@gmail.com>
2021-02-26 15:09:46 -0500

    passing onadd as prop in products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -14,7 +14,7 @@ const Products = ({ products, onAddToCart }) => {
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart}/>
           </Grid>
         ))}
       </Grid>
