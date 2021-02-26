47d80a58b8f5b708805ebdcb29fd5225529ce13c
Ellie Lambert <ebezjian@gmail.com>
2021-02-26 15:30:39 -0500

    add grid to filled cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import {Container, Typography, Button, Grid } from '@material-ui/core';
 
-const Cart = () => {
-  const isEmpty = true;
+const Cart = ({cart}) => {
+  const isEmpty =  !cart.line_items.length;
   const EmptyCart = () => (
     <Typography variant="subtitle1">You have no items in your shopping cart, get to browsing!</Typography>
   );
   const FilledCart = () => (
     <>
       <Grid container spacing={3}>
-        
+        {cart.line_items.map((item) =>(
+          <Grid item xs={12} sm={4} key={item.id}>
+            <div>{item.name}</div>
+
+          </Grid>
+        ))}
       </Grid>
     </>
   );
@@ -17,7 +22,7 @@ const Cart = () => {
     <Container>
       <div className={classes.toolbar}/>
       <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
-      {isEmpty? <EmptyCart/>: <FilledCart />}
+      {isEmpty ? <EmptyCart/> : <FilledCart />}
     </Container>
   )
 }
