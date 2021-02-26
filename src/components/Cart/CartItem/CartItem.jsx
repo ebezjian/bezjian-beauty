4e3dcc5716bd76368aecdc5892111cd5c3c470dd
Ellie Lambert <ebezjian@gmail.com>
2021-02-26 16:45:17 -0500

    add cartitem folder as child to cart and styles.js with quick styling and boilerplate code in jsx

---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography, Button, Card, CardActions, CardContent, CardMedia} from '@material-ui/core';
+import usestyles from './styles';
+
+
+
+const CartItem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CartItem
