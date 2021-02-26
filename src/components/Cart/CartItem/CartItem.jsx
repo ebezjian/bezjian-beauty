e7b441dd4adad4af604d0a3c480b2f781994e914
Ellie Lambert <ebezjian@gmail.com>
2021-02-26 16:53:27 -0500

    add item total and button to card

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -9,6 +9,15 @@ const CartItem = ({item}) => {
   return (
     <Card>
       <CardMedia image={item.media.source} alt={item.name} className={classes.media}/>
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="h5">{item.line_total.formatted_with_symbol}</Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}>
+
+        </div>
+      </CardActions>
     </Card>
   )
 }
