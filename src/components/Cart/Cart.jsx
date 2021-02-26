1960881958b8ab0645d9de93723724a761126ca0
Ellie Lambert <ebezjian@gmail.com>
2021-02-26 15:35:06 -0500

    add buttons to cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -12,10 +12,19 @@ const Cart = ({cart}) => {
         {cart.line_items.map((item) =>(
           <Grid item xs={12} sm={4} key={item.id}>
             <div>{item.name}</div>
-
           </Grid>
         ))}
       </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4">
+          Subtotal: {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <div>
+          <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
+          <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+        </div>
+
+      </div>
     </>
   );
   return (
