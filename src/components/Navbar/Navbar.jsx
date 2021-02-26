d8ff671b15afede41d87e2aa4a42bfb23ebede0c
Ellie Lambert <ebezjian@gmail.com>
2021-02-26 17:18:41 -0500

    add same link but for homepage to logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = ({totalItems}) => {
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
-          <Typography variant="h6" className={classes.title} color="inherit">
+          <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
             <img src={logo} alt="Commerce.js" height="25px" className={classes.image}/>
             Bezjian Beauty
           </Typography>
