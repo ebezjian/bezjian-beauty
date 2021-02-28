67e8fc315bdf09664b46129ddcf0514111bd9c96
Ellie Lambert <ebezjian@gmail.com>
2021-02-28 17:33:32 -0500

    adjustments to homepage for parallax

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,34 +1,44 @@
 import React from "react";
 
-import { Parallax } from "react-parallax";
+import { Parallax, Background} from 'react-parallax';
 
 
 const styles = {
   fontFamily: "sans-serif",
-  textAlign: "center"
+  textAlign: "center",
+  
 };
 const insideStyles = {
-  background: "white",
-  padding: 20,
+  // background: "white",
+  padding: 0,
   position: "absolute",
   top: "50%",
   left: "50%",
+  
   transform: "translate(-50%,-50%)"
 };
 
 const image1 =
-  "https://cdn.discordapp.com/attachments/537816642122547212/815641335440605234/banner1.jpg";
+"https://images.pexels.com/photos/3373738/pexels-photo-3373738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+
+
   const image2 =
-  "https://cdn.discordapp.com/attachments/537816642122547212/815641411918889051/logo2.png";
+  "https://images.unsplash.com/photo-1484311969987-0a20cd9afedc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
 
 const Home = () => (
-  <div style={styles}>
-    
-    <Parallax bgImage={image1} strength={500}>
-      <div style={{ height: 500 }}>
-        <div style={insideStyles}>{image2}</div>
-      </div>
-    </Parallax>
-  </div>)
+  
+
+  
+
+
+<div style={styles}>
+  <h2>| | |</h2>
+   <Parallax bgImage={image2} strength={-100}>
+    <div style={{ height: 500 }}>
+      <div style={insideStyles}>Reverse direction</div>
+    </div>
+    </Parallax> 
+  </div> 
+  )
 
 export default Home
