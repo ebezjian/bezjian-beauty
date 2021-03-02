35c198cba11664def7eba280efac38e472c343dc
Ellie Lambert <ebezjian@gmail.com>
2021-03-02 11:26:16 -0500

    trying to get pages to show correctly

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
+  const [home, setHome] = useState();
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = ({totalItems}) => {
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
           <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
-            <img src={logo} alt="Commerce.js" height="25px" className={classes.image}/>
+            <img src={logo} alt="Bezjian Beauty Logo" height="25px" className={classes.image}/>
             Bezjian Beauty
           </Typography>
           <div className={classes.grow}/>
