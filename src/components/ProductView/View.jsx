b78f2308ecad4da5e3f8eb43dd81d9f7944c76ee
Ellie Lambert <ebezjian@gmail.com>
2021-03-08 13:12:19 -0500

    add const to start doing local state to view

--- src/components/ProductView/View.jsx
+++ src/components/ProductView/View.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import useStyles from './styles';
 
-const View = () => {
+const View = ({addProduct}) => {
   const classes = useStyles();
+  const [product, setProduct] = useState({});
+  const [quantity, setQuantity] = useState(1);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProduct = async (id) => {
+    const response = await commerce.products.retrieve(id);
+    const {name, price, media, quantity, description} = response;
+    setProduct({name, price, media, quantity, description, src: media.source, price: price.formatted_with_symbol,});
+  };
   return (
     <div>
       
