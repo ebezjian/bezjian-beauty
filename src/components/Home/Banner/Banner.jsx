aeee5b55e7263a14ec80ff8a8647ecd7ff6cd518
Ellie Lambert <ebezjian@gmail.com>
2021-03-09 11:20:44 -0500

    small adjustments for placement in banner

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 import {Container, Button, Grid} from "@material-ui/core";
-import logo from '../../assets/Bezjian.png';
-import makeup from '../../assets/makeup.jpg';
+import logo from '../../../assets/Bezjian.png';
+import makeup from '../../../assets/makeup.jpg';
 import "./styles.css";
 
 const Banner = () => {
@@ -8,7 +8,7 @@ const Banner = () => {
     <div className="banner">
       <Container>
         <Grid container spacing={4}>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={6} className="logo">
             <img src={logo} alt="Bezjian-beauty"/>
             <Button className="shopping-Button" href="#products">
               Start Shopping!
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Banner from './Banner/Banner';
 import { Parallax,} from 'react-parallax';
 import Splash from './Splash/SplashPage';
 
@@ -19,26 +19,24 @@ const insideStyles = {
   transform: "translate(-50%,-50%)"
 };
 
-
-
-
-
-
 const image2 =
 "https://images.unsplash.com/photo-1484311969987-0a20cd9afedc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
 const Home = () => (
   
   
 <div>
-    <div>
+  <div>
+    <Banner/>
+  </div>
+  <div>
     <Splash/>
   </div>
-<div style={styles}>
-  <h2>| | |</h2>
-   <Parallax bgImage={image2} strength={-100}>
-    <div style={{ height: 500 }}>
-      <div style={insideStyles}>Reverse direction</div>
-    </div>
+  <div style={styles}>
+    <h2>| | |</h2>
+    <Parallax bgImage={image2} strength={-100}>
+      <div style={{ height: 500 }}>
+        <div style={insideStyles}>Reverse direction</div>
+      </div>
     </Parallax> 
   </div>
 
