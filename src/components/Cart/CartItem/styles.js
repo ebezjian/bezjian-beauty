c82e58ef56b091f1a4386c381c55ad41043af742
Ellie Lambert <ebezjian@gmail.com>
2021-03-10 18:11:31 -0500

    adding styling to cards for shopping cart

--- src/components/Cart/CartItem/styles.js
+++ src/components/Cart/CartItem/styles.js
@@ -1,12 +1,20 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
+  root: {
+    // maxWidth: 345, original width style
+    maxWidth: '100%',
+    
+    color: 'white'
+  },
   media: {
-    height: 260,
+    minHeight: 500,
   },
   cardContent: {
     display: 'flex',
     justifyContent: 'space-between',
+    color: 'white',
+    backgroundImage: 'linear-gradient(#cc5973,#e0cfd3)',
   },
   cartActions: {
     justifyContent: 'space-between',
