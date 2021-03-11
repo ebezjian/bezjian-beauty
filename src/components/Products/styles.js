4c72084f3b24a16307e935792e1919e71874e8ec
Ellie Lambert <ebezjian@gmail.com>
2021-03-11 15:57:46 -0500

    quick background image to products page

--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -1,11 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
-
+import Background from '../../assets/wallpaper.png';
 export default makeStyles((theme) => ({
   toolbar: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing(3),
+    backgroundImage: `url(${Background})`
   },
   root: {
     flexGrow: 1,
